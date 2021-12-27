7b0624c58c32cc513cf76ba71c9f23fb7095017a
Ajay S <69749439+Ajay-S77@users.noreply.github.com>
2021-12-27 12:36:56 +0530

    pulled request

--- eclipseDemo/src/main/java/eclipseDemo/Test.java
+++ eclipseDemo/src/main/java/eclipseDemo/Test.java
@@ -5,6 +5,7 @@ public class Test {
 		System.out.println("Test completed chanage 1");
 		System.out.println("ChANGES DONE BY JOHN");
 		System.out.println("new change");
+                System.out.println("request pulled");
 		
 	}
 
