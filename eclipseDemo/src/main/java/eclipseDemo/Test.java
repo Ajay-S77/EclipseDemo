ee9fde60316e83e823dcf25d6891cc652995a1cd
Ajay S77 <ajayjohn1999@gmail.com>
2021-12-27 12:16:22 +0530

    new change

--- eclipseDemo/src/main/java/eclipseDemo/Test.java
+++ eclipseDemo/src/main/java/eclipseDemo/Test.java
@@ -4,7 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Test completed chanage 1");
 		System.out.println("ChANGES DONE BY JOHN");
-		System.out.println("thirddd change");
+		System.out.println("new change");
 		
 	}
 
