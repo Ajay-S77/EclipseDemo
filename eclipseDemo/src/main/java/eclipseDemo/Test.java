5cda921b15b2155f03f5f471b6481ecb5e1eb7f5
Ajay S <69749439+Ajay-S77@users.noreply.github.com>
2021-12-27 11:41:24 +0530

    John Changes

--- eclipseDemo/src/main/java/eclipseDemo/Test.java
+++ eclipseDemo/src/main/java/eclipseDemo/Test.java
@@ -3,6 +3,8 @@ package eclipseDemo;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("Test completed chanage 1");
+		System.out.println("ChANGES DONE BY JOHN");
+		
 	}
 
 }
