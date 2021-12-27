343b39355fd65d5c0e9e92c7a1c127ecbd09c2db
Ajay S77 <ajayjohn1999@gmail.com>
2021-12-27 11:39:04 +0530

    Second commit

--- eclipseDemo/src/main/java/eclipseDemo/Test.java
+++ eclipseDemo/src/main/java/eclipseDemo/Test.java
@@ -2,7 +2,7 @@ package eclipseDemo;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("Test completed");
+		System.out.println("Test completed chanage 1");
 	}
 
 }
